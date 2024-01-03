3869390cc11d4e9154bc9895eaf41ee4223c0860
huun0424 <huunl91@naver.com>
2024-01-04 00:07:52 +0900

    - feat: emotion css props 설정.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const titleStyle = () => css`
+  color: blue;
+`;
 
 function App() {
-  return <div>react init</div>;
+  return <div css={titleStyle}>react init!</div>;
 }
 
 export default App;
